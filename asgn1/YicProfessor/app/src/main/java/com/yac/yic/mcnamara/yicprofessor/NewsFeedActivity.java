628b1711bb330896dcff5eb971433b852657b629
Walton Seymour <bigw369@gmail.com>
2014-09-23 11:47:28 -0500

    removed setting power button

--- asgn1/YicProfessor/app/src/main/java/com/yac/yic/mcnamara/yicprofessor/NewsFeedActivity.java
+++ asgn1/YicProfessor/app/src/main/java/com/yac/yic/mcnamara/yicprofessor/NewsFeedActivity.java
@@ -70,9 +70,7 @@ public class NewsFeedActivity extends ListActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
         if (id == R.id.action_new) {
             // Add prompt for text input here
             return true;
