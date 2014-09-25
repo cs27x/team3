4425d4de7eecf172fc99af1db793fc8acaf82427
sportnak <david.m.nakayama@vanderbilt.edu>
2014-09-25 12:05:48 -0500

    remove unwanted menu options

--- asgn1/YicProfessor/app/src/main/java/com/yac/yic/mcnamara/yicprofessor/TextPrompt.java
+++ asgn1/YicProfessor/app/src/main/java/com/yac/yic/mcnamara/yicprofessor/TextPrompt.java
@@ -28,18 +28,6 @@ public class TextPrompt extends Activity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
     //callback to post to the list.
     public void onPost(View view) {
         newProfessor = (EditText)findViewById(R.id.professor);
